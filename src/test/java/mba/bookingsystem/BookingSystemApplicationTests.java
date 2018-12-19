edfc863abdb7337e458446b266f76a39c6da5fe4
macieg_b <bartlomiejczyk.maciej@gmail.com>
2018-12-19 06:01:31 +0100

    Project initial commit

---
+++ src/test/java/mba/bookingsystem/BookingSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package mba.bookingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
