b41b7c549c329f3e4d2e8f3cdc716c11f88ed9b1
macieg_b <bartlomiejczyk.maciej@gmail.com>
2018-12-22 13:36:17 +0100

    Add lombok

--- src/main/java/mba/bookingsystem/model/db/Organization.java
+++ src/main/java/mba/bookingsystem/model/db/Organization.java
@@ -1,39 +1,22 @@
 package mba.bookingsystem.model.db;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Organization {
     @Id
     @GeneratedValue
     private UUID uuid;
     private String name;
 
-    public Organization() {
-
-    }
-
-    public Organization(UUID uuid, String name) {
-        this.uuid = uuid;
-        this.name = name;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/mba/bookingsystem/model/view/OrganizationView.java
+++ src/main/java/mba/bookingsystem/model/view/OrganizationView.java
@@ -1,37 +1,22 @@
 package mba.bookingsystem.model.view;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.UUID;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class OrganizationView {
 
     private UUID uuid;
     @NotNull
     @Size(min = 2, max = 20)
     private String name;
-
-    public OrganizationView() {
-    }
-
-    public OrganizationView(UUID uuid, @NotNull @Size(min = 2, max = 20) String name) {
-        this.uuid = uuid;
-        this.name = name;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
